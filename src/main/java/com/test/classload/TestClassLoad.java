04bc32f7c235e42b6ee71d0836d6389631db22d9
li.jinlong <li.jinlong@ielpm.com>
2019-04-18 16:27:11 +0800

    享元模式

--- src/main/java/com/test/classload/TestClassLoad.java
+++ src/main/java/com/test/classload/TestClassLoad.java
@@ -4,7 +4,8 @@ public class TestClassLoad {
 
 	public static void main(String[] args) {
 //		System.out.println(TestClassLoad.class.getClassLoader().toString());
-		
+
 		System.out.println(System.getProperty("java.ext.dirs"));
+
 	}
 }
